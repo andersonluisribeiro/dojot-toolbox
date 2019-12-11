1e246b79b321e5a5695bcbee45f9e76e645a5984
Anderson Luis Ribeiro <anderson.luis.rib@gmail.com>
2019-12-11 10:40:15 -0300

    testing kong

--- lib/kong.py
+++ lib/kong.py
@@ -38,8 +38,8 @@ class Kong(Component):
 
     @property
     def vars(self):
-        self._vars['apigw_req_per_minute'] = self.__req_per_minute
-        self._vars['apigw_req_per_hour'] = self.__req_per_minute
-        self._vars['apigw_pg_username'] = self.__pg_username
-        self._vars['apigw_pg_password'] = self.__pg_password
+        self._vars['dojot_kong_req_per_minute'] = self.__req_per_minute
+        self._vars['dojot_kong_req_per_hour'] = self.__req_per_hour
+        self._vars['dojot_psql_kong_user'] = self.__pg_username
+        self._vars['dojot_psql_kong_passwd'] = self.__pg_password
         return self._vars
